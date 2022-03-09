37078ed517d1fd861748c7b697596eea0f60f868
dvgff <1261462367@qq.com>
2022-03-09 11:25:10 +0800

    update schedule_job

--- schedule-use-admin/src/main/java/com/jfeat/use/admin/services/domain/service/ScheduleJobRecordService.java
+++
@@ -1,9 +0,0 @@
-package com.jfeat.use.admin.services.domain.service;
-
-import com.jfeat.use.admin.services.gen.crud.service.CRUDScheduleJobRecordService;
-
-/**
- * Created by vincent on 2017/10/19.
- */
-public interface ScheduleJobRecordService extends CRUDScheduleJobRecordService {
-    }
