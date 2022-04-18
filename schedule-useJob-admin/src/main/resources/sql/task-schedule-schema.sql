392125335b7e96ea059c9bb6bcb3a6a380bbc1d5
林学文 <826982262@qq.com>
2022-04-18 16:25:06 +0800

    update sqlFile

--- schedule-useJob-admin/src/main/resources/sql/task-schedule-schema.sql
+++ schedule-useJob-admin/src/main/resources/sql/task-schedule-schema.sql
@@ -30,9 +30,23 @@ CREATE TABLE `t_schedule_record`  (
   `use_player_id` bigint(20) NULL DEFAULT NULL COMMENT '运行人id',
   `session_id` bigint(20) NULL DEFAULT NULL COMMENT '场次id',
   `schedule_number` bigint(20) NULL DEFAULT NULL COMMENT '定时任务记录编号',
-  `job_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '定时任务名',
+  `job_name` varchar(32) NOT NULL COMMENT '定时任务名',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `schedule_number`(`schedule_number`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 302457 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  UNIQUE (`schedule_number`)
+) ENGINE = InnoDB AUTO_INCREMENT = 302457 ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `t_schedule_job_record`;
+CREATE TABLE `t_schedule_job_record`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `job_name` varchar(200) NULL DEFAULT NULL COMMENT '定时任务名字(英文大写)',
+  `job_group_name` varchar(200) NULL DEFAULT NULL COMMENT '定时任务组名',
+  `job_class` varchar(200) NULL DEFAULT NULL COMMENT '任务',
+  `cron` varchar(200) NULL DEFAULT NULL COMMENT '时间设置',
+  `delete_flag` tinyint(4) NULL DEFAULT 0 COMMENT '删除逻辑',
+  `duplicate_task` varchar(20) NULL DEFAULT NULL COMMENT '子任务名字',
+  `seq` int(4) NULL DEFAULT 1 COMMENT '子任务数',
+  `allow_repeat` tinyint(4) NULL DEFAULT 0 COMMENT '是否可重复执行项，1可重复执行',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
